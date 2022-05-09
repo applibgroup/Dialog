2c8fc03e226bf4d10c80b2b1a05750496ba5cc9f
Pasindu-Mihiran <mihiranp481@gmail.com>
2022-05-09 13:50:02 +0530

    Building Dialog

---
+++ Material_Dialog/entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,15 @@
+export default {
+    data: {
+        title: 'World'
+    },
+    alert: function(){
+        this.$element('main_alert').show();
+    }
+//    showpopup() {
+//        this.$element("popup").show();
+//    },
+//    hidepopup() {
+//        this.$element("popup").hide();
+//    },
+
+}
